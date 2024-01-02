9d13770474f99583795dacb22e282a8851805bca
John Arevalo <johnarevalo@gmail.com>
2024-01-02 17:03:31 -0500

    Fix indexing

--- correct/mnn.py
+++ correct/mnn.py
@@ -1,3 +1,4 @@
+import pandas as pd
 import numpy as np
 
 from quality_control import io
@@ -7,13 +8,22 @@ def mnn(parquet_path, batch_key, output_path):
     from mnnpy import mnn_correct, settings
     # disabling parallel because it silently freezes the execution
     settings.normalization = 'no_parallel'
+
     meta, vals, features = io.split_parquet(parquet_path)
-    col = meta[batch_key]
-    indices = col.groupby(col, observed=True, sort=False).indices
-    vals = [vals[ix] for ix in indices.values]
+
+    # group values
+    col = pd.Series(data=meta.index, index=meta[batch_key])
+    indices = list(col.groupby(level=0).indices.values())
+    vals = [vals[ix] for ix in indices]
+
+    # correct
     vals, _, __ = mnn_correct(*vals, var_index=features, do_concatenate=True)
 
-    indices = np.concatenate(indices.values)
-    meta = meta.iloc[indices]
+    # Recover original order for vals
+    indices = np.concatenate(indices)
+    sort_ix = np.argsort(indices)
+    vals = vals[sort_ix]
+
+    # Save file
     features = [f'mnn_{i}' for i in range(vals.shape[1])]
     io.merge_parquet(meta, vals, features, output_path)
--- correct/scanorama.py
+++ correct/scanorama.py
@@ -1,3 +1,4 @@
+import pandas as pd
 import numpy as np
 import scanpy as sc
 from quality_control import io
@@ -24,11 +25,21 @@ def scanorama(parquet_path, batch_key, output_path):
     '''Scanorama correction without pca'''
     from scanorama import assemble
     meta, vals, features = io.split_parquet(parquet_path)
-    col = meta[batch_key]
-    indices = col.groupby(col, observed=True, sort=False).indices
-    vals = [vals[ix] for ix in indices.values]
+
+    # group values
+    col = pd.Series(data=meta.index, index=meta[batch_key])
+    indices = list(col.groupby(level=0).indices.values())
+    vals = [vals[ix] for ix in indices]
+
+    # correct
     vals = assemble(vals)
     vals = np.concatenate(vals)
-    indices = np.concatenate(indices.values)
-    meta = meta.iloc[indices]
+
+    # Recover original order for vals
+    indices = np.concatenate(indices)
+    sort_ix = np.argsort(indices)
+    vals = vals[sort_ix]
+
+    # Save file
+    features = [f'scanorama_{i}' for i in range(vals.shape[1])]
     io.merge_parquet(meta, vals, features, output_path)
