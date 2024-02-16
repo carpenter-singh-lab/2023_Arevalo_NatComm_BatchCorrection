f4ef550caa2ea047792c0433cb18f8fdce4523d3
John Arevalo <johnarevalo@gmail.com>
2024-02-16 16:49:26 +0000

    Update runtime plots

--- plot/load_runtimes.py
+++ plot/load_runtimes.py
@@ -1,8 +1,10 @@
 import base64
+import datetime
 import json
 from glob import glob
 from io import BytesIO
 
+import matplotlib
 import pandas as pd
 import seaborn as sns
 from matplotlib import pyplot as plt
@@ -20,29 +22,29 @@ def show_inline(close=False):
 
 
 categories = {
-    "nmi": "metric",
-    "ari": "metric",
-    "mean_average_precision": "metric",
-    "pcr": "metric",
-    "average_precision_negcon": "metric",
-    "compute_negcon_stats": "preprocessing",
-    "combat": "method",
-    "select_variant_feats": "preprocessing",
-    "pcr_batch": "metric",
-    "average_precision_nonrep": "metric",
-    "lisi_batch": "metric",
-    "sphering_explore": "method",
-    "mad_normalize": "preprocessing",
-    "write_parquet": "preprocessing",
-    "silhouette_batch": "metric",
-    "featselect": "preprocessing",
-    "INT": "preprocessing",
-    "clustering": "metric",
-    "graph_conn": "metric",
-    "asw": "metric",
-    "harmony": "method",
-    "lisi_label": "metric",
-    "mnn": "method",
+    "nmi": "Metric",
+    "ari": "Metric",
+    "mean_average_precision": "Metric",
+    "pcr": "Metric",
+    "average_precision_negcon": "Metric",
+    "compute_negcon_stats": "Preprocessing",
+    "combat": "Method",
+    "select_variant_feats": "Preprocessing",
+    "pcr_batch": "Metric",
+    "average_precision_nonrep": "Metric",
+    "lisi_batch": "Metric",
+    "sphering_explore": "Method",
+    "mad_normalize": "Preprocessing",
+    "write_parquet": "Preprocessing",
+    "silhouette_batch": "Metric",
+    "featselect": "Preprocessing",
+    "INT": "Preprocessing",
+    "clustering": "Metric",
+    "graph_conn": "Metric",
+    "asw": "Metric",
+    "harmony": "Method",
+    "lisi_label": "Metric",
+    "mnn": "Method",
 }
 
 
@@ -53,7 +55,7 @@ def load_json(f: str):
 
 files = glob(".snakemake/metadata/*")
 df = pd.DataFrame(map(load_json, files))
-df["runtime"] = df.eval("endtime - starttime")
+df["runtime"] = df.eval("(endtime - starttime)")
 df["category"] = df["rule"].map(categories)
 order = df.groupby("rule")["runtime"].mean().sort_values().index
 order = pd.Series(index=order, data=range(len(order)))
@@ -62,7 +64,21 @@ df = (
     .sort_values(by="category", kind="stable")
     .dropna(subset="category")
 )
-g = sns.barplot(df, x="rule", y="runtime", hue="category")
-g.set_yscale("log")
+df["Scenario"] = df.input.apply(" ".join).str.extract(r"(scenario_.)", expand=False)
+ax = sns.barplot(df, x="rule", y="runtime", hue="category")
+ax.set_yscale("log")
+
+
+def timeTicks(x, pos):
+    d = datetime.timedelta(seconds=x)
+    return str(d)
+
+
+formatter = matplotlib.ticker.FuncFormatter(timeTicks)
+ax.yaxis.set_major_formatter(formatter)
+ax.set_ylabel("Runtime (hours)")
+ax.set_xlabel("Operation")
+ax.get_legend().set_title("Category")
+
 plt.xticks(rotation=45, ha="right")
 show_inline(True)
